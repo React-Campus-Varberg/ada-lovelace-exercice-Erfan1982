55093314a7ed088e3aeadf31e64bf3bf4cca9ca9
Erfan <erfantavoosi@gmail.com>
2020-11-04 15:08:11 +0100

    version 1 av Ada Lovelace

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function App() {
+
+    const message = 'Ada Lovelace';
+
+    return <h2>{message}</h2>;
+
+    export default App;
+}
---
+++ Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ada from './ada.jpg';
+
+function Image() {
+
+    return <img src={ada}></img>
+
+
+}
+
+export default Image;
